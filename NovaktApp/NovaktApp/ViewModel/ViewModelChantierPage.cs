018c3d3a77c728995316d4c56f43fec81586bcf1
Valentin <v.lemaitre@iia-laval.fr>
2017-05-29 12:05:22 +0200

    Ajout ListeChantierPage + ChantierPage

---
+++ NovaktApp/NovaktApp/ViewModel/ViewModelChantierPage.cs
@@ -0,0 +1,16 @@
+﻿using NovaktApp.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NovaktApp.ViewModel
+{
+    public class ViewModelChantierPage
+    {
+        public ViewModelChantierPage(INavigation nav, Chantier ch)
+        {
+
+        }
+    }
+}
