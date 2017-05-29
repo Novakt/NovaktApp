297683a0128c65efc591e9a4c7835dddc9bb2cc3
antoinerenault53 <antoine.renault53@laposte.net>
2017-05-29 13:41:39 +0200

    Antoine Modif

---
+++ NovaktApp/NovaktApp/ViewModel/ViewModelDetailProduitPage.cs
@@ -0,0 +1,46 @@
+﻿using NovaktApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace NovaktApp.ViewModel
+{
+    public class ViewModelDetailProduitPage : Observable
+    {
+        private INavigation _Navigation;
+        private Produit _SelectProduit;
+        private ObservableCollection<Produit> _Produit;
+
+        //Produit selectionné dans la liste
+        public Produit SelectProduit
+        {
+            get { return _Produit; }
+            set
+            {
+               
+            }
+        }
+
+        //Permet de naviguer entre les pages
+        public INavigation Navigation
+        {
+            get { return _Navigation; }
+            set
+            {
+                OnPropertyChanging(nameof(Navigation));
+                _Navigation = value;
+                OnPropertyChanged(nameof(Navigation));
+
+            }
+        }
+
+        public ViewModelListProduitPage(INavigation nav)
+        {
+            
+
+            _Navigation = nav;
+        }
+    }
+}
