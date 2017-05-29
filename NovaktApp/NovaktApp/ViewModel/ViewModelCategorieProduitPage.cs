d9369de489c1a58393213f4aaba648f99e36ae77
antoinerenault53 <antoine.renault53@laposte.net>
2017-05-29 10:30:50 +0200

    Correctif Page XAML

--- NovaktApp/NovaktApp/ViewModel/ViewModelCategorieProduitPage.cs
+++ NovaktApp/NovaktApp/ViewModel/ViewModelCategorieProduitPage.cs
@@ -1,16 +1,20 @@
 ﻿using NovaktApp.Core;
+using NovaktApp.Entity;
 using NovaktApp.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 
 namespace NovaktApp.ViewModel
 {
-    class ViewModelCategorieProduitPage
+    class ViewModelCategorieProduitPage : Observable
     {
         private INavigation _Navigation;
-        private DelegateCommand _ConnexionCommand;
+        private Categorie _SelectCategorie;
+        private ObservableCollection<Categorie> _Categories;
+        private DelegateCommand _ListCategorieCommand;
 
         public INavigation Navigation
         {
@@ -24,28 +28,69 @@ namespace NovaktApp.ViewModel
                 _Navigation = value;
             }
         }
-        public DelegateCommand ConnexionCommand => _ConnexionCommand;
+        //public DelegateCommand ConnexionCommand => _ConnexionCommand;
 
         public ViewModelCategorieProduitPage(INavigation nav)
         {
             _Navigation = nav;
 
-            _ConnexionCommand = new DelegateCommand(ExecuteConnexionCommand);
+            _ListCategorieCommand = new DelegateCommand(ExecuteListProduitCommand);
 
+            Categories = new ObservableCollection<Categorie>();
+
+            Categorie cat1 = new Categorie();
+            Categorie cat2 = new Categorie();
+            Categorie cat3 = new Categorie();
+            Categorie cat4 = new Categorie();
+            Categorie cat5 = new Categorie();
+
+            cat1.Nom = "Catégorie 1";
+            cat2.Nom = "Catégorie 2";
+            cat3.Nom = "Catégorie 3";
+            cat4.Nom = "Catégorie 4";
+            cat5.Nom = "Catégorie 5";
+
+            Categories.Add(cat1);
+            Categories.Add(cat2);
+            Categories.Add(cat3);
+            Categories.Add(cat4);
+            Categories.Add(cat5);
         }
 
-        private void ExecuteConnexionCommand(object obj)
+        //Catégorie selectionné dans la liste
+        public Categorie SelectCategorie
         {
-            //verification login/password
-            //TODO
+            get { return _SelectCategorie; }
+            set
+            {
+                OnPropertyChanging(nameof(SelectCategorie));
+                _SelectCategorie = value;
+                OnPropertyChanged(nameof(SelectCategorie));
 
-            // vers page Accueil
+            }
+        }
+
+        //Liste des catégories
+        public ObservableCollection<Categorie> Categories
+        {
+            get { return _Categories; }
+            set
+            {
+                OnPropertyChanging(nameof(Categories));
+                _Categories = value;
+                OnPropertyChanged(nameof(Categories));
+
+            }
+        }
+
+        private void ExecuteListProduitCommand(object obj)
+        {
+            //Permt de naviguer vers la page estimation
             ListeProduitPage pg = new ListeProduitPage();
-            ViewModelCategorieProduitPage vm = new ViewModelCategorieProduitPage(pg.Navigation);
+            ViewModelListProduitPage vm = new ViewModelListProduitPage(pg.Navigation);
             pg.BindingContext = vm;
             this._Navigation.PushAsync(pg).ConfigureAwait(false);
-
-
         }
+
     }
 }
